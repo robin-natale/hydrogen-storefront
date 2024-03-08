1985a5d2353f2b9400c6fb91a6c48a7f97e3b795
Robin <robin.natale@swankyagency.com>
2024-03-08 10:35:27 +0000

    Add loader data to interact with server

--- app/routes/_index.jsx
+++ app/routes/_index.jsx
@@ -1,7 +1,20 @@
+import { useLoaderData } from "@remix-run/react";
+import { json } from "@shopify/remix-oxygen";
+
+// only on the server
+export async function loader() {
+  return json({
+    message: "Hello World from server",
+  })
+}
+
+// only on the client - can interact with server data
 export default function Index() {
+    // eslint-disable-next-line prettier/prettier
+    const { message } = useLoaderData();
     return (
       <div>
-        <h1>Hello World</h1>
+        <h1>{ message }</h1>
         <p>Welcome to the new hydrogen test store</p>
       </div>
     );
