f981225bc07fb075db45561e2c76039f50da7386
Robin <robin.natale@swankyagency.com>
2024-03-08 11:24:58 +0000

    Progress in getting data from server

--- app/routes/_index.jsx
+++ app/routes/_index.jsx
@@ -1,21 +1,40 @@
-import {useLoaderData} from '@remix-run/react';
+import {useLoaderData, Link} from '@remix-run/react';
 import {json} from '@shopify/remix-oxygen';
 
 // only on the server
-export async function loader() {
+export async function loader({context}) {
+  const {products} = context.storefront.query(ALL_PRODUCTS_QUERY);
   return json({
-    message: 'Hello World from server',
+    // FIXME: Cannot read properties of undefined (reading 'nodes')
+     products: products.nodes,
   });
 }
 
 // only on the client - can interact with server data
 export default function Index() {
   // eslint-disable-next-line prettier/prettier
-    const { message } = useLoaderData();
+    const { products } = useLoaderData();
   return (
     <div>
-      <h1>{message}</h1>
-      <p>Welcome to the new hydrogen test store</p>
+      <h1>Products</h1>
+      {products.map((product) => (
+        <Link key={product.handle} to={`/products/${product.handle}`}>
+          {product.title}
+        </Link>
+      ))}
     </div>
   );
 }
+
+const ALL_PRODUCTS_QUERY = `#graphql
+ query AllProducts{
+    products(first: 10) {
+        nodes {
+        title
+        description
+        id
+        handle
+      }
+    }
+ }
+`;
