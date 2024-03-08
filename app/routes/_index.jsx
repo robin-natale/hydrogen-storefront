62ac6d7f5989baf7073a8e626735a442b9aceae0
Robin <robin.natale@swankyagency.com>
2024-03-08 10:36:59 +0000

    Fix prettier problems

--- app/routes/_index.jsx
+++ app/routes/_index.jsx
@@ -1,21 +1,21 @@
-import { useLoaderData } from "@remix-run/react";
-import { json } from "@shopify/remix-oxygen";
+import {useLoaderData} from '@remix-run/react';
+import {json} from '@shopify/remix-oxygen';
 
 // only on the server
 export async function loader() {
   return json({
-    message: "Hello World from server",
-  })
+    message: 'Hello World from server',
+  });
 }
 
 // only on the client - can interact with server data
 export default function Index() {
-    // eslint-disable-next-line prettier/prettier
+  // eslint-disable-next-line prettier/prettier
     const { message } = useLoaderData();
-    return (
-      <div>
-        <h1>{ message }</h1>
-        <p>Welcome to the new hydrogen test store</p>
-      </div>
-    );
+  return (
+    <div>
+      <h1>{message}</h1>
+      <p>Welcome to the new hydrogen test store</p>
+    </div>
+  );
 }
